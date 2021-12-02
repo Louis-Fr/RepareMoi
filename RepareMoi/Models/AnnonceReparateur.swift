218efaa1d43a64aa183e0036a5019e35afbc4005
Lefantastique59 <troplolo@ymail.com>
2021-12-02 16:02:03 +0100

    Ajout Annonces réparateurs

---
+++ RepareMoi/Models/AnnonceReparateur.swift
@@ -0,0 +1,16 @@
+//
+//  AnnonceRéparateur.swift
+//  RepareMoi
+//
+//  Created by Apprenant 39 on 02/12/2021.
+//
+
+import Foundation
+
+import SwiftUI
+
+struct AnnonceReparateur {
+    let profil: Profil
+    let competence: [CompetenceAppareil:Int]
+    let disponibilite: Disponibilite
+}
