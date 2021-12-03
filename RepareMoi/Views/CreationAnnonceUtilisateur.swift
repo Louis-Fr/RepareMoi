0a66d73b9151ced2c3529b5f3a5dbb504490ec10
Louis-Fr <louisreypro@yahoo.com>
2021-12-03 13:52:09 +0100

    Ajout CreationAnnonceUtilisateur

---
+++ RepareMoi/Views/CreationAnnonceUtilisateur.swift
@@ -0,0 +1,20 @@
+//
+//  CreationAnnonceUtilisateur.swift
+//  RepareMoi
+//
+//  Created by Apprenant 62 on 03/12/2021.
+//
+
+import SwiftUI
+
+struct CreationAnnonceUtilisateur: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreationAnnonceUtilisateur_Previews: PreviewProvider {
+    static var previews: some View {
+        CreationAnnonceUtilisateur()
+    }
+}
