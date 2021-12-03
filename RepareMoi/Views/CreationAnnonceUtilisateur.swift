c09fa5e44aa7440e584861b7c67ea87c2c17f264
Louis-Fr <louisreypro@yahoo.com>
2021-12-03 13:54:35 +0100

    Maj CreationAnnonceUtilisateur

--- RepareMoi/Views/CreationAnnonceUtilisateur.swift
+++ RepareMoi/Views/CreationAnnonceUtilisateur.swift
@@ -8,8 +8,43 @@
 import SwiftUI
 
 struct CreationAnnonceUtilisateur: View {
+    @State private var description: String = "Description du problème"
+    @State private var statutToggle = true
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+            Text("Description")
+                .font(.headline)
+                .bold()
+            TextField("Description", text: $description)
+                .padding()
+                .textFieldStyle(.roundedBorder)
+                .foregroundColor(.gray)
+            
+            
+            
+            VStack {
+                Text("Compétences")
+                    .font(.headline)
+                    .bold()
+                Toggle("Ordinateur", isOn: $statutToggle)
+                    .padding()
+                    .toggleStyle(SwitchToggleStyle(tint: .blue))
+                Toggle("Smartphone", isOn: $statutToggle)
+                    .padding()
+                    .toggleStyle(SwitchToggleStyle(tint: .blue))
+                Toggle("Tablette", isOn: $statutToggle)
+                    .toggleStyle(SwitchToggleStyle(tint: .blue))
+                    .padding()
+                Toggle("Autre", isOn: $statutToggle)
+                    .toggleStyle(SwitchToggleStyle(tint: .blue))
+                    .padding()
+                
+            } // Fin Vstack 2
+            
+        } // Fin Vstack 1
+        
     }
 }
 
@@ -18,3 +53,4 @@ struct CreationAnnonceUtilisateur_Previews: PreviewProvider {
         CreationAnnonceUtilisateur()
     }
 }
+
