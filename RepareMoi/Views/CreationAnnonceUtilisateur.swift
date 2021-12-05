78f406e94e75f81682f6952458f4bf6db2831ee4
Louis Rey <louisreypro@yahoo.com>
2021-12-05 15:37:34 +0100

    Maj CreationAnnonceUtilisateur
    Ajout Toggle et Picker fonctionnel

--- RepareMoi/Views/CreationAnnonceUtilisateur.swift
+++ RepareMoi/Views/CreationAnnonceUtilisateur.swift
@@ -11,8 +11,10 @@ struct CreationAnnonceUtilisateur: View {
     
     
     @State private var descriptionProbleme: String = "Description du problème"
-    @State private var statutToggle = true
-    //@State private var TypeAppareil = AppareilCategory.self
+    @State private var statutToggleSemaine = false
+    @State private var statutToggleWeekend = false
+    @State private var statutToggleUrgent = false
+    @State private var typeAppareil = false
     
     var body: some View {
         
@@ -22,13 +24,18 @@ struct CreationAnnonceUtilisateur: View {
                 Text("Type d'appareil")
                     .font(.headline)
                     .bold()
-                Picker("Type Appareil", selection: $statutToggle)  {
+                Picker("Type Appareil", selection: $typeAppareil)  {
                     ForEach(AppareilCategory.allCases) { Appareils in
                         Text(Appareils.rawValue.capitalized)
                             .tag(Appareils)
                             
                     }
+
                 } // Fin Picker
+                Spacer()
+                Spacer()
+                Spacer()
+                Spacer()
                     
                 Text("Description")
                     .font(.headline)
@@ -41,33 +48,52 @@ struct CreationAnnonceUtilisateur: View {
                     .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.blue, lineWidth: 1))
                     .frame(width: 300, height: 45)
                     .padding(8)
-                /* TextField("Description", text: $description)
-                    .padding()
-                    .textFieldStyle(.roundedBorder)
-                    .foregroundColor(.gray) */
-                
-                
+            } // Fin Vstack Description
+            
+            .padding()
+            
+            
+            Spacer()
+            Spacer()
+            Spacer()
+            Spacer()
                 
                 VStack {
-                    Text("Compétences")
+                    
+                    
+                    Text("Disponibilité")
                         .font(.headline)
                         .bold()
-                    Toggle("Ordinateur", isOn: $statutToggle)
+                    HStack {
+                        //Text("Urgent")
+                            //.font(.headline)
+                            //.bold()
+                            //.frame(maxWidth: 180, maxHeight: 50, alignment: .leading)
+                            //.foregroundColor(Color.red)
+                        Toggle("Urgent", isOn: $statutToggleUrgent)
+                            .padding()
+                            .toggleStyle(SwitchToggleStyle(tint: .blue))
+                            .foregroundColor(Color.red)
+                            //.frame(width: 0, alignment: .trailing)
+                    }
+                    Toggle("Semaine", isOn: $statutToggleSemaine)
                         .padding()
                         .toggleStyle(SwitchToggleStyle(tint: .blue))
-                    Toggle("Smartphone", isOn: $statutToggle)
+                    Toggle("Week-end", isOn: $statutToggleWeekend)
                         .padding()
                         .toggleStyle(SwitchToggleStyle(tint: .blue))
-                    Toggle("Tablette", isOn: $statutToggle)
+                   /* Toggle("Tablette", isOn: $statutToggle)
                         .toggleStyle(SwitchToggleStyle(tint: .blue))
                         .padding()
                     Toggle("Autre", isOn: $statutToggle)
                         .toggleStyle(SwitchToggleStyle(tint: .blue))
-                        .padding()
+                        .padding() */
                     
                 } // Fin Vstack Compétences
+            
+            
                  
-            } // Fin Vstack Description
+            
             
         } // Fin ScrollView
         
