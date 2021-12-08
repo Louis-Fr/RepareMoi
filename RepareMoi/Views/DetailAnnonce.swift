33048d3bd67807489d5bdde5cfdaa46320c3aa2a
Louis Rey <louisreypro@yahoo.com>
2021-12-08 14:21:23 +0100

    add DetailAnnonce

---
+++ RepareMoi/Views/DetailAnnonce.swift
@@ -0,0 +1,20 @@
+//
+//  DetailAnnonce.swift
+//  RepareMoi
+//
+//  Created by Louis REY on 08/12/2021.
+//
+
+import SwiftUI
+
+struct DetailAnnonce: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailAnnonce_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailAnnonce()
+    }
+}
