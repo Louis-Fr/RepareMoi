b1000d9860d7cc9e46a9590f7f22f3120ab6675c
Louis Rey <louisreypro@yahoo.com>
2021-12-09 15:56:15 +0100

    addAppareil View

---
+++ RepareMoi/Views/addAppareil.swift
@@ -0,0 +1,20 @@
+//
+//  addAppareil.swift
+//  RepareMoi
+//
+//  Created by Louis REY on 09/12/2021.
+//
+
+import SwiftUI
+
+struct addAppareil: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct addAppareil_Previews: PreviewProvider {
+    static var previews: some View {
+        addAppareil()
+    }
+}
