8cc43967f8d6af61c3ea7014014a2b8ce5121372
Louis Rey <louisreypro@yahoo.com>
2021-12-12 18:56:13 +0100

    maj AddAppareil
    Modification AddAppareil

--- RepareMoi/Views/addAppareil.swift
+++ RepareMoi/Views/addAppareil.swift
@@ -18,51 +18,106 @@ struct addAppareil: View {
     var body: some View {
         
         NavigationView {
-            VStack {
-                Text("Quel type d'appareil voulez-vous ajouter?")
-                    .padding()
+    
+                ZStack {
+                    Color("BaseColor")
+                        .ignoresSafeArea()
+                
+                    VStack(alignment: .center){
+                    
+                    Text("Scanner votre appareil")
+                        .padding()
+                        .font(Font.system(size: 18, weight: .bold))
+                    NavigationLink(destination: AppareilUser(),
+                                   label: {
+                        Image(systemName: "camera.viewfinder")
+                            .font(.system(size: 40))
+                    })
+                            .padding(35)
+                    
+                        
+                    
+                    
+                    Text("Quel type d'appareil voulez-vous ajouter ?")
+                        .padding()
+                        .font(Font.system(size: 18, weight: .medium))
 
-                Picker("Type Appareil", selection: $choiceAppareil) {
-                    ForEach(AppareilCategory.allCases) { choixTypeAppareil in
-                        Text(choixTypeAppareil.rawValue.capitalized)
-                            .tag(choixTypeAppareil)
-                    }
-                } // Fin Picker
-                
-                Text("Dans quel état était votre appareil lors de l'achat?")
-                    .padding()
-                
-                Picker("Type Appareil", selection: $choicePicker) {
-                    ForEach(TypeAchat.allCases) { TypeAchat in
-                        Text(TypeAchat.rawValue.capitalized)
-                            .tag(TypeAchat)
-                    }
-                } // Fin Picker
-                
-                Text("Quand a t'il été acheté?")
-                    .padding()
-                
-                Picker("Date achat", selection: $choiceDateAchat) {
-                        Text("1 ans")
-                        Text("2 ans")
-                        Text("3 ans")
-                        Text("4 ans")
-                        Text("5 ans")
-                        Text("6 ans")
-                     
-                            //.tag(DateAchat)
-                    }
-                } // Fin Picker
-                
-                
-            }
+                    Picker("Type Appareil", selection: $choiceAppareil) {
+                        ForEach(AppareilCategory.allCases) { choixTypeAppareil in
+                            Text(choixTypeAppareil.rawValue.capitalized)
+                                .tag(choixTypeAppareil)
+                        }
+                       
+                    } // Fin Picker
+                    .padding(15)
+                    .background(Color.white)
+                    .background(.bar)
+                    .cornerRadius(50)
+                    
+                    Text("Dans quel état était votre appareil lors de l'achat ?")
+                        .padding()
+                        .font(Font.system(size: 18, weight: .medium))
+                    
+                    Picker("Type Appareil", selection: $choicePicker) {
+                        ForEach(TypeAchat.allCases) { TypeAchat in
+                            Text(TypeAchat.rawValue.capitalized)
+                                .tag(TypeAchat)
+                        }
+                       
+                    } // Fin Picker
+                    .padding(15)
+                    .background(Color.white)
+                    .background(.bar)
+                    .cornerRadius(50)
+                    
+                    Text("Quand l'avez-vous acheté ?")
+                        .padding()
+                        .font(Font.system(size: 18, weight: .medium))
+                        
+                    
+                    Picker("Date achat", selection: $choiceDateAchat) {
+                            Text("1 ans")
+                            Text("2 ans")
+                            Text("3 ans")
+                            Text("4 ans")
+                            Text("5 ans")
+                            Text("6 ans")
+                         
+                                //.tag(DateAchat)
+                        }
+                    .padding(15)
+                    .background(Color.white)
+                    .background(.bar)
+                    .cornerRadius(50)
+                        
+                        Button(action: {
+                            print("Appareil ajouté")
+                        }) {
+                            Text("Ajouter mon appareil")
+                                .padding(15)
+                                .background(Color.white)
+                                .background(.bar)
+                                .cornerRadius(50)
+                        }
+                        .padding(45)
+                        
+                    } // Fin Picker
+                    
+                } // Fin Zstack
+               
+                    
+                } // Fin Vstack
+           
             
-            .navigationTitle("Ajouter un appareil")
-            .navigationBarTitleDisplayMode(.inline)
-        }
+           
+            
+            
+            
+            } // Fin NavigationView
+            
+        } // Fin View body
         
-        
-    }
+
 
 
 struct addAppareil_Previews: PreviewProvider {
